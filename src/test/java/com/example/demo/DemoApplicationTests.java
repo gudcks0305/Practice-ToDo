75c57d1431509ea3db553d4019863f97dd0cacc1
yoo hyeong chan <gudcks305@gmail.com>
2022-12-13 17:37:26 +0900

    feature : mysql -> maria db

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class DemoApplicationTests {
 
