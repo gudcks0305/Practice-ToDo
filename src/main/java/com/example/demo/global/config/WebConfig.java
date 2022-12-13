f9458ba7908a8b149da48173c322ebf3dbf0f8e4
yoo hyeong chan <gudcks305@gmail.com>
2022-12-13 15:21:18 +0900

    test : cloudType terminal

--- src/main/java/com/example/demo/global/config/WebConfig.java
+++ src/main/java/com/example/demo/global/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOriginPatterns("*")
+                        .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
                         //.allowCredentials(true);
             }
